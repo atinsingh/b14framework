77ca4cabab95fd99aea10d0539e535fcdd6284ac
Singh, Atin <>
2019-09-24 20:56:55 -0400

    Added DriverManager

---
+++ src/test/java/co/pragra/testingframework/config/Constants.java
@@ -0,0 +1,9 @@
+package co.pragra.testingframework.config;
+
+public class Constants {
+    public final static String CHROME ="chrome";
+    public final static String FIREFOX ="firefox";
+    public final static String OPERA ="opera";
+    public final static String IE ="ie";
+    public final static String EDGE ="edge";
+}
--- src/test/java/co/pragra/testingframework/drivermanager/DriverManager.java
+++ src/test/java/co/pragra/testingframework/drivermanager/DriverManager.java
@@ -1,4 +1,43 @@
 package co.pragra.testingframework.drivermanager;
 
+import co.pragra.testingframework.config.Configuration;
+import co.pragra.testingframework.config.Constants;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * This class manage WebDriver instance
+ * How many instance of driver we needed
+ */
 public class DriverManager {
+
+    private WebDriver driver;
+    private static DriverManager manager;
+
+    private DriverManager(){
+        driver = createDriver();
+    }
+
+
+    private WebDriver createDriver(){
+        if(Configuration.getInstance().getProperty("browser").equals(Constants.CHROME)){
+            return new ChromeDriver();
+        }else if(Configuration.getInstance().getProperty("browser").equals(Constants.FIREFOX)){
+            return new FirefoxDriver();
+        }else {
+            return new ChromeDriver();
+        }
+    }
+
+    private WebDriver getDriver() {
+        return driver;
+    }
+    public static WebDriver getWebDriver(){
+        if(manager==null){
+            manager = new DriverManager();
+            return manager.getDriver();
+        }
+        return manager.getDriver();
+    }
 }
