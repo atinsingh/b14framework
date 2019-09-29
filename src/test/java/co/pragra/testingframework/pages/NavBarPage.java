fc8acdacb9b55e3dbe7cdec698b4e0e327a9ea05
atiyap <55922579+atiyap@users.noreply.github.com>
2019-09-28 20:18:07 -0400

    Update NavBarPage.java

--- src/test/java/co/pragra/testingframework/pages/NavBarPage.java
+++ src/test/java/co/pragra/testingframework/pages/NavBarPage.java
@@ -1,8 +1,6 @@
 package co.pragra.testingframework.pages;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
@@ -13,42 +11,22 @@ public class NavBarPage {
 
     @FindBy(css = "li[class= 'dropdown mobile-hide']>a[id='btnSolutions']")
     private WebElement solutions;
-// @FindBy(css = "a[href*=\"zoomrooms\"]")
+
     @FindBy(css = "a[href*='zoomrooms']")
     private WebElement roomsAndWorkspaces;
-
-
-
-=======
->>>>>>> 6316963b81f07a429434421adf0eccff1ef575d8
-=======
-
->>>>>>> d1e7da24fd616cf2c9f2c16edcd198ea52e4cae9
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-<<<<<<< HEAD
-=======
-import org.openqa.selenium.support.ui.ExpectedCondition;
->>>>>>> d1e7da24fd616cf2c9f2c16edcd198ea52e4cae9
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class NavBarPage {
-<<<<<<< HEAD
-    private WebDriver driver;
-   // public  WebDriverWait wait = new WebDriverWait(driver, 20);
 
     @FindBy (css = "li[class='dropdown mobile-hide']>a[id='btnSolutions']")
     private WebElement solutions;
 
-   // @FindBy (css ="a[href='\\meetings']")
-
     @FindBy(css = "a[href*='meetings']")
     //@FindBy(css = "a[href=\"https://zoom.us/phonesystem\"]")
     private WebElement meetingsandchat;
+    
+    @FindBy(xpath = "//*[@id=\"btnSolutions\"]")
+    private  WebElement Solution;
+
+    @FindBy(xpath="//*[@id='first-col-nav']/div/a[4]")
+    private  WebElement VideoWeb;
 
     public NavBarPage(WebDriver driver) {
         this.driver = driver;
@@ -65,38 +43,13 @@ public class NavBarPage {
 
     }
 
-}
-=======
-    private  WebDriver driver;
-
-
-//    @FindBy(css="id= navbar")
-    @FindBy(xpath = "//*[@id=\"btnSolutions\"]")
-    private  WebElement Solution;
-
-//    @FindBy(css = "a[href='\\pricing'")
-
-    @FindBy(xpath="//*[@id='first-col-nav']/div/a[4]")
-    private  WebElement VideoWeb;
->>>>>>> d1e7da24fd616cf2c9f2c16edcd198ea52e4cae9
-
-    public NavBarPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver,this);
-    }
-
-
-<<<<<<< HEAD
     public RoomsAndWorkspaces clickRoomsAndWorkspaces(){
         Actions actions = new Actions(driver);
         actions.moveToElement(solutions).build().perform();
        actions.moveToElement(roomsAndWorkspaces).click().build().perform();
         return new RoomsAndWorkspaces(driver);
 
-
 }
-}
-=======
 
     public VideoWebinars clickVideoWebinars(){
         Actions actions=new Actions(driver);
@@ -108,18 +61,6 @@ public class NavBarPage {
         return new VideoWebinars(driver);
     }
 
-
-
-
-
-
-
-
-
-
-
 }
 
 
-
->>>>>>> d1e7da24fd616cf2c9f2c16edcd198ea52e4cae9
